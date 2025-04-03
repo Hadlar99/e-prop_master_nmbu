da1de8b7fd95b14cbc90b5b6cab744db7f664969
Harald N. Stabbetorp <stabbe@hotmail.no>
2025-04-03 16:11:38 +0200

    increased iterations

--- code_accumelated/eprop_0_1_pca_45.py
+++ code_accumelated/eprop_0_1_pca_45.py
@@ -34,7 +34,7 @@ np.random.seed(rng_seed)  # fix numpy random seed
 
 # Define timing task
 n_batch = 64  # batch size, 64 in reference [2], 32 in the README to reference [2]
-n_iter = 300  # number of iterations, 2000 in reference [2], 50 with n_batch 32 converges
+n_iter = 400  # number of iterations, 2000 in reference [2], 50 with n_batch 32 converges
 
 n_input_symbols = 2  # number of input populations, e.g. 4 = left, right, recall, noise
 n_cues = 3  # number of cues given before decision
