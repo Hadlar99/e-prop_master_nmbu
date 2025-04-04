bd656c0e7038c053fe20cc95d1eb16a2680bd30c
Harald N. Stabbetorp <stabbe@hotmail.no>
2025-04-04 09:34:24 +0200

    increaed network and reduced iterations

--- code_accumelated/eprop_0_1_2_pca_45.py
+++ code_accumelated/eprop_0_1_2_pca_45.py
@@ -33,8 +33,8 @@ rng_seed = 1  # numpy random seed
 np.random.seed(rng_seed)  # fix numpy random seed
 
 # Define timing task
-n_batch = 32  # batch size, 64 in reference [2], 32 in the README to reference [2]
-n_iter = 400  # number of iterations, 2000 in reference [2], 50 with n_batch 32 converges
+n_batch = 64  # batch size, 64 in reference [2], 32 in the README to reference [2]
+n_iter = 300  # number of iterations, 2000 in reference [2], 50 with n_batch 32 converges
 
 n_input_symbols = 2  # number of input populations, e.g. 4 = left, right, recall, noise
 n_cues = 3  # number of cues given before decision
@@ -87,8 +87,8 @@ nest.set(**params_setup)
 
 ### Create neurons
 n_in = 30  # number of input neurons
-n_ad = 60  # number of adaptive neurons
-n_reg = 60  # number of regular neurons
+n_ad = 120  # number of adaptive neurons
+n_reg = 120  # number of regular neurons
 n_rec = n_ad + n_reg  # number of recurrent neurons
 n_out = 3  # number of readout neurons
 
