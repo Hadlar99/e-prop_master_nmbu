7a5f9c7fe73f6748b09d9029373b6591769e8618
Harald N. Stabbetorp <stabbe@hotmail.no>
2025-04-17 15:42:23 +0200

    fixed spelling error

--- code_accumelated/orion_scripts_val_45/val_0_1_pca_45_120.py
+++ code_accumelated/orion_scripts_val_45/val_0_1_pca_45_120.py
@@ -336,7 +336,7 @@ def apply_loaded_weights(csv_path):
             print(f"Error applying weight: {e}")
 
 
-apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_45/results_2025-04-17_120_neurons/weights_120.csv")
+apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_45/results_2025-04-17_120_neruons/weights_120.csv")
 
 ## Create input and output spike generators
 data = {}
