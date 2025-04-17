42473e8fa4d303e6095a326b0134c29bc7d44b6b
Harald N. Stabbetorp <stabbe@hotmail.no>
2025-04-17 20:55:53 +0200

    fixed path

--- code_accumelated/orion_scripts_val_5/val_0_1_pca_5_240.py
+++ code_accumelated/orion_scripts_val_5/val_0_1_pca_5_240.py
@@ -336,7 +336,7 @@ def apply_loaded_weights(csv_path):
             print(f"Error applying weight: {e}")
 
 
-apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_5/results_2025-04-17_240_neurons/weights_5_240.csv")
+apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_5/results_2025-04-17_5_240_neurons/weights_5_240.csv")
 
 ## Create input and output spike generators
 data = {}
--- code_accumelated/orion_scripts_val_5/val_0_1_pca_5_60.py
+++ code_accumelated/orion_scripts_val_5/val_0_1_pca_5_60.py
@@ -336,7 +336,7 @@ def apply_loaded_weights(csv_path):
             print(f"Error applying weight: {e}")
 
 
-apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_5/results_2025-04-17_60_neurons/weights_5_60.csv")
+apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_training_5/results_2025-04-17_5_60_neurons/weights_5_60.csv")
 
 ## Create input and output spike generators
 data = {}
