8ec68e4f4f42e86ecfde8f9125fc4426217a6a39
Harald N. Stabbetorp <stabbe@hotmail.no>
2025-05-01 17:06:13 +0200

    Ensure that variables is int

--- code_accumelated/orion_scripts_train_4_8_val/val_4_8_pca_45_240_list_2.py
+++ code_accumelated/orion_scripts_train_4_8_val/val_4_8_pca_45_240_list_2.py
@@ -342,8 +342,9 @@ apply_loaded_weights("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/or
 list_train = pd.read_csv("/mnt/users/hastabbe/e-prop_master_nmbu/code_accumelated/orion_scripts_train_4_8_val/list2.csv")
 
 # Create a list of numbers from the csv file
-train_number = list_train["New List"].tolist()
+samples_list = list_train["New List"].tolist()
 
+train_number = [int(num) for num in samples_list]
 ## Create input and output spike generators
 raw_data = {}
 all_samples = []
